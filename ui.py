6a5a15214f39472a70684a53d864dbc57cf7afdb
Jose <jose123ma@hotmail.com>
2023-09-12 13:45:40 -0400

    Layout 2nd pass

--- ui.py
+++ ui.py
@@ -19,12 +19,34 @@ def show_ui():
 
     cmds.window(WINDOW_NAME, title='UV Editing Tool', widthHeight=(500,100))
 
-    cmds.gridLayout(autoGrow=True,numberOfRowsColumns=(3,2),cellWidthHeight=(200,60))
-    cmds.button(label='1000000000000000000')
-    cmds.button(label='1')
-    cmds.button(label='1')
-    cmds.text(label='100000000000000000')
-    cmds.text(label='1')
-    cmds.text(label='1')
+    cmds.rowLayout(numberOfColumns=3,adjustableColumn3=3)
+    cmds.text(label='\n1 [base]\n\n2 [edit]\n\n\n\n3 [size]\n\n\n')
+
+    cmds.columnLayout(adjustableColumn=True)
+    cmds.button(CAMERA_BASED_BUTTON_NAME,label='Camera-based')
+
+    cmds.rowLayout(numberOfColumns=2)
+    cmds.button(CUT_SEW_BUTTON_NAME,label='Cut/Sew\nTool',recomputeSize=True)
+    cmds.button(UNFOLD_BUTTON_NAME,label='Unfold',recomputeSize=True)
+    cmds.setParent('..')
+
+    cmds.button(LAYOUT_BUTTON_NAME,label='Layout',recomputeSize=True)
+    cmds.text(label='\n\n\n')
+
+    cmds.setParent('..')
+
+    cmds.columnLayout(adjustableColumn=True)
+
+    cmds.button(AUTO_UNWRAP_BUTTON_NAME,label='Automatic',recomputeSize=True)
+
+    cmds.text(label='\n\n')
+    cmds.rowLayout(numberOfColumns=2)
+    cmds.button(TILEABLE_1M_BUTTON_NAME, label='Tileable 1M\n(10.24|1024)',recomputeSize=True)
+    cmds.button(TILEABLE_2M_BUTTON_NAME, label='Tileable 2M\n(10.24|2048)',recomputeSize=True)
+    cmds.setParent('..')
+    cmds.rowLayout(numberOfColumns=3,adjustableColumn3=1)
+    cmds.textField(CUSTOM_DENSITY_TEXTBOX_NAME)
+    cmds.textField(CUSTOM_MAP_SIZE_TEXTBOX_NAME)
+    cmds.button(TILEABLE_CUSTOM_BUTTON_NAME, label='Set Custom\nTexel Density',recomputeSize=True)
 
     cmds.showWindow()
