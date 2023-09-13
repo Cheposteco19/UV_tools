bbc1d359b1df280b41233439c286dec3d4f198c3
Jose <jose123ma@hotmail.com>
2023-09-13 19:47:09 -0400

    Layout 3rd iteration

--- ui.py
+++ ui.py
@@ -6,7 +6,6 @@ CAMERA_BASED_BUTTON_NAME='camera_based_button'
 CUT_SEW_BUTTON_NAME='cut_sew_button'
 UNFOLD_BUTTON_NAME='unfold_button'
 AUTO_UNWRAP_BUTTON_NAME='auto_unwrap_button'
-LAYOUT_BUTTON_NAME='layout_button'
 TILEABLE_1M_BUTTON_NAME='tileable_1m_button'
 TILEABLE_2M_BUTTON_NAME='tileable_2m_button'
 TILEABLE_CUSTOM_BUTTON_NAME='tileable_custom_button'
@@ -17,36 +16,33 @@ def show_ui():
     if cmds.window(WINDOW_NAME,query=True,exists=True):
         cmds.deleteUI(WINDOW_NAME)
 
-    cmds.window(WINDOW_NAME, title='UV Editing Tool', widthHeight=(500,100))
-
-    cmds.rowLayout(numberOfColumns=3,adjustableColumn3=3)
-    cmds.text(label='\n1 [base]\n\n2 [edit]\n\n\n\n3 [size]\n\n\n')
+    cmds.window(WINDOW_NAME, title='UV Tools', widthHeight=(250,175))
 
     cmds.columnLayout(adjustableColumn=True)
+    cmds.rowLayout(numberOfColumns=2)
+    cmds.columnLayout(adjustableColumn=True, backgroundColor=(.1, .1, .2))
+    cmds.text(label='BAKED',font='boldLabelFont')
     cmds.button(CAMERA_BASED_BUTTON_NAME,label='Camera-based')
-
-    cmds.rowLayout(numberOfColumns=2)
-    cmds.button(CUT_SEW_BUTTON_NAME,label='Cut/Sew\nTool',recomputeSize=True)
-    cmds.button(UNFOLD_BUTTON_NAME,label='Unfold',recomputeSize=True)
+    cmds.button(CUT_SEW_BUTTON_NAME,label='Cut/Sew\nTool',height=38)
+    cmds.button(UNFOLD_BUTTON_NAME,label='Unfold',height=47)
+    cmds.text(label='',height=35)
     cmds.setParent('..')
 
-    cmds.button(LAYOUT_BUTTON_NAME,label='Layout',recomputeSize=True)
-    cmds.text(label='\n\n\n')
+    cmds.columnLayout(adjustableColumn=True,backgroundColor=(.1,.2,.1))
+    cmds.text(label='TILED', font='boldLabelFont')
+    cmds.button(AUTO_UNWRAP_BUTTON_NAME,label='Automatic')
+    cmds.rowLayout(numberOfColumns=2)
 
+    cmds.columnLayout(adjustableColumn=True)
+    cmds.button(TILEABLE_1M_BUTTON_NAME, label='Tileable 1M\n(10.24|1024)')
+    cmds.text(label='Texel density\n(px/inch)')
+    cmds.textField(CUSTOM_DENSITY_TEXTBOX_NAME,width=10,text='10.24')
+    cmds.text(label='Map size')
+    cmds.textField(CUSTOM_MAP_SIZE_TEXTBOX_NAME,width=10,text='4096')
     cmds.setParent('..')
 
     cmds.columnLayout(adjustableColumn=True)
-
-    cmds.button(AUTO_UNWRAP_BUTTON_NAME,label='Automatic',recomputeSize=True)
-
-    cmds.text(label='\n\n')
-    cmds.rowLayout(numberOfColumns=2)
-    cmds.button(TILEABLE_1M_BUTTON_NAME, label='Tileable 1M\n(10.24|1024)',recomputeSize=True)
-    cmds.button(TILEABLE_2M_BUTTON_NAME, label='Tileable 2M\n(10.24|2048)',recomputeSize=True)
-    cmds.setParent('..')
-    cmds.rowLayout(numberOfColumns=3,adjustableColumn3=1)
-    cmds.textField(CUSTOM_DENSITY_TEXTBOX_NAME)
-    cmds.textField(CUSTOM_MAP_SIZE_TEXTBOX_NAME)
-    cmds.button(TILEABLE_CUSTOM_BUTTON_NAME, label='Set Custom\nTexel Density',recomputeSize=True)
+    cmds.button(TILEABLE_2M_BUTTON_NAME, label='Tileable 2M\n(10.24|2048)')
+    cmds.button(TILEABLE_CUSTOM_BUTTON_NAME, label='Custom\nTexel\nDensity',height=80)
 
     cmds.showWindow()
