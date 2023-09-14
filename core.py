a13b8761a9fe351ca428d83ff5e55180e452f662
Jose <jose123ma@hotmail.com>
2023-09-14 19:10:45 -0400

    Functionality plugged

--- core.py
+++ core.py
@@ -15,13 +15,11 @@ def unfold(*args):
     selected_items=cmds.ls(selection=True)
     for item in selected_items:
         cmds.u3dUnfold(item,ite=True,p=False,bi=True,tf=True,ms=1024,rs=False)
+    mm.eval("texOrientShells;")
+    cmds.u3dLayout(selected_items)
 
 def set_cut_sew_tool(*args):
     cmds.SetCutSewUVTool()
-
-def auto_layout(*args):
-    selected_items=cmds.ls(selection=True)
-    cmds.u3dLayout(selected_items)
 
 def set_tileable_size(density,map_size):
     mm.eval("texSetTexelDensity {} {};".format(density,map_size))
--- ui.py
+++ ui.py
@@ -1,5 +1,5 @@
 from maya import cmds
-from uv_editing_tool import core
+from uv_editing_tool import core as uv_editing_core
 
 WINDOW_NAME='uv_editing_tool_ui'
 CAMERA_BASED_BUTTON_NAME='camera_based_button'
@@ -18,37 +18,53 @@ def show_ui():
 
     cmds.window(WINDOW_NAME, title='UV Tools', widthHeight=(250,185))
 
+    #Baked column
     cmds.columnLayout(adjustableColumn=True)
     cmds.rowLayout(numberOfColumns=2)
     cmds.columnLayout(adjustableColumn=True, backgroundColor=(.1, .1, .2))
     cmds.text(label='BAKED',font='boldLabelFont')
-    cmds.button(CAMERA_BASED_BUTTON_NAME,label='Camera-based')
-    cmds.button(CUT_SEW_BUTTON_NAME,label='Cut/Sew\nTool',height=38)
-    cmds.button(UNFOLD_BUTTON_NAME,label='Unfold',height=47)
+    cmds.button(CAMERA_BASED_BUTTON_NAME,label='Camera-based',command=uv_editing_core.camera_based)
+    cmds.button(CUT_SEW_BUTTON_NAME,label='Cut/Sew\nTool',height=38,command=uv_editing_core.set_cut_sew_tool)
+    cmds.button(UNFOLD_BUTTON_NAME,label='Unfold',height=47,command=uv_editing_core.unfold)
     cmds.text(label='',height=35)
     cmds.setParent('..')
 
+    #Tiled column
     cmds.columnLayout(adjustableColumn=True,backgroundColor=(.1,.2,.1))
     cmds.text(label='TILED', font='boldLabelFont')
-    cmds.button(AUTO_UNWRAP_BUTTON_NAME,label='Automatic')
+    cmds.button(AUTO_UNWRAP_BUTTON_NAME,label='Automatic',command=uv_editing_core.auto_unwrap)
     cmds.rowLayout(numberOfColumns=2)
 
+    #Density first column
     cmds.columnLayout(adjustableColumn=True)
-    cmds.button(TILEABLE_1M_BUTTON_NAME, label='Tileable 1M\n(10.24|1024)')
+    cmds.button(TILEABLE_1M_BUTTON_NAME, label='Tileable 1M\n(10.24|1024)',command=texel_density_1m)
     cmds.text(label='Texel density\n(px/inch)')
     cmds.textField(CUSTOM_DENSITY_TEXTBOX_NAME,width=10,text='10.24')
     cmds.text(label='Map size')
     cmds.textField(CUSTOM_MAP_SIZE_TEXTBOX_NAME,width=10,text='4096')
     cmds.setParent('..')
 
+    #Density second column
     cmds.columnLayout(adjustableColumn=True)
-    cmds.button(TILEABLE_2M_BUTTON_NAME, label='Tileable 2M\n(10.24|2048)')
-    cmds.button(TILEABLE_CUSTOM_BUTTON_NAME, label='Custom\nTexel\nDensity',height=80)
+    cmds.button(TILEABLE_2M_BUTTON_NAME, label='Tileable 2M\n(10.24|2048)',command=texel_density_2m)
+    cmds.button(TILEABLE_CUSTOM_BUTTON_NAME, label='Custom\nTexel\nDensity',height=80,command=texel_density_custom)
     cmds.setParent('..')
     cmds.setParent('..')
     cmds.setParent('..')
     cmds.setParent('..')
 
+    #Credits
     cmds.text(label='GD67_JoseMunguia   ', align='right')
 
     cmds.showWindow()
+
+def texel_density_1m(*args):
+    uv_editing_core.set_tileable_size(10.24,1024)
+
+def texel_density_2m(*args):
+    uv_editing_core.set_tileable_size(10.24,2048)
+
+def texel_density_custom(*args):
+    density=cmds.textField(CUSTOM_DENSITY_TEXTBOX_NAME,query=True,text=True)
+    map_size=cmds.textField(CUSTOM_MAP_SIZE_TEXTBOX_NAME,query=True,text=True)
+    uv_editing_core.set_tileable_size(density,map_size)
