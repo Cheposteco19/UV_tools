e9ebe8685ae25904f945e955e2dc42a56c7ecd7c
Jose <jose123ma@hotmail.com>
2023-09-26 19:32:27 -0400

    Camera based bug, after selection features added

--- core.py
+++ core.py
@@ -3,19 +3,35 @@ import maya.mel as mm
 
 def auto_unwrap(*args):
     selected_items=cmds.ls(selection=True)
+    objects = []
     for item in selected_items:
-        cmds.polyAutoProjection(item)
+        if objects.append(item.split('.')[0]) not in objects:
+            objects.append(item.split('.')[0])
+    for item in objects:
+        face_index = cmds.polyEvaluate(item, face=True) - 1
+        faces = '{}.f[0:{}]'.format(item, face_index)
+        cmds.polyAutoProjection(faces)
+    cmds.selectMode(object=True)
+    cmds.select(objects,replace=True)
 
 def camera_based(*args):
     selected_items = cmds.ls(selection=True)
     new_selection=[]
+    objects = []
     for item in selected_items:
-        object=item.split('.')[0]
-        face_index=cmds.polyEvaluate(item,face=True)-1
-        new_selection.append('{}.f[0:{}]'.format(object,face_index))
-    print(new_selection)
+        if objects.append(item.split('.')[0]) not in objects:
+            objects.append(item.split('.')[0])
+    for item in objects:
+        face_index = cmds.polyEvaluate(item,face=True)-1
+        new_selection.append('{}.f[0:{}]'.format(item,face_index))
+    cmds.select(clear=True)
+    cmds.select(objects, replace=True)
+    cmds.hilite(objects)
     cmds.select(new_selection,replace=True)
     cmds.polyProjection(type='Planar', mapDirection='p', constructionHistory=True)
+    #Select object again
+    cmds.selectMode(object=True)
+    cmds.select(objects,replace=True)
 
 def unfold(*args):
     selected_items=cmds.ls(selection=True)
@@ -29,17 +45,17 @@ def set_cut_sew_tool(*args):
 
 def set_tileable_size(density,map_size):
     selected_items = cmds.ls(selection=True)
-    print(selected_items)
     new_selection = []
     objects=[]
+    final_selection=[]
     for item in selected_items:
         if objects.append(item.split('.')[0]) not in objects:
             objects.append(item.split('.')[0])
-    print(objects)
     for item in objects:
         map_index = cmds.polyEvaluate(item, uvcoord=True) - 1
+        edge_index = cmds.polyEvaluate(item, edge=True) - 1
         new_selection.append('{}.map[0:{}]'.format(item, map_index))
-        print(map_index)
+        final_selection.append('{}.e[0:{}]'.format(item, edge_index))
     cmds.select(clear=True)
     cmds.select(objects,replace=True)
     cmds.hilite(objects)
@@ -51,3 +67,7 @@ def set_tileable_size(density,map_size):
     mm.eval("texUnstackShells 1;")
     cmds.DeleteHistory()
     cmds.FreezeTransformations()
+
+    cmds.selectMode(edge=True)
+    cmds.hilite(objects)
+    #cmds.select(final_selection, replace=True)
