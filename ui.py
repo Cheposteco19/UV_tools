69638fac6a1fd348846a797612bb8c66fbbdcd5d
Jose <jose123ma@hotmail.com>
2023-12-04 17:07:47 -0600

    Version update

--- ui.py
+++ ui.py
@@ -67,7 +67,7 @@ def show_ui():
 
     #Credits
     cmds.rowLayout(numberOfColumns=2,adjustableColumn=2)
-    cmds.text(label='V 1.0.3')
+    cmds.text(label='V 1.1.0')
     cmds.text(label='GD67_JoseMunguia   ', align='right')
 
     cmds.showWindow()
